503705748e0e21c0e8b72793a5984b3b617f75bc
mrkir <mrkirill046@yandex.ru>
2025-07-25 00:28:33 +0300

    updated player design

---
+++ src/components/icons/custom-circle-pause.svelte
@@ -0,0 +1,24 @@
+<script>
+    import {cn} from "@/lib/utils"
+
+    export let size = 24
+</script>
+
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    class={cn($$props.class, "lucide-icon lucide")}
+    width={size}
+    height={size}
+    fill="currentColor"
+>
+    <defs>
+        <mask id="pause-mask">
+            <rect width="24" height="24" fill="white" />
+            <rect x="9" y="8" width="2" height="8" fill="black" />
+            <rect x="13" y="8" width="2" height="8" fill="black" />
+        </mask>
+    </defs>
+
+    <circle cx="12" cy="12" r="10" mask="url(#pause-mask)" />
+</svg>
--- src/components/shared/controls.svelte
+++ src/components/shared/controls.svelte
@@ -1,46 +1,74 @@
 <script lang="ts">
     import {pauseAudio, restartAudio, resumeAudio} from "@/lib/player"
-    import {cover, currentPath} from "@/stores/audio-store"
+    import {cover, currentPath, isPlaying} from "@/stores/audio-store"
+    import {Ban, Heart, RotateCcw, SkipBack, SkipForward} from "@lucide/svelte"
     import {quadInOut} from "svelte/easing"
     import {fade} from "svelte/transition"
+    import CustomCirclePlay from "@/components/icons/custom-circle-play.svelte"
+    import CustomCirclePause from "@/components/icons/custom-circle-pause.svelte"
+    import {cn} from "@/lib/utils"
 </script>
 
-<div class="flex flex-col items-center justify-center" in:fade={{duration: 150, easing: quadInOut}}>
+<div class="flex flex-col items-center justify-center gap-y-5" in:fade={{duration: 150, easing: quadInOut}}>
     <article>
-        {#if $cover}
-            <img src={$cover} alt="Cover" class="object-cover rounded-xl w-48 h-48" />
-        {:else}
-            <div></div>
-        {/if}
-    </article>
+        <div class="flex flex-col w-full h-full px-4 gap-y-4">
+            {#if $cover}
+                <img
+                    src={$cover}
+                    alt="Cover"
+                    class={cn(
+                        "object-cover rounded-xl transition-transform duration-300 ease-in-out w-[40rem] h-[40rem]",
+                        $isPlaying ? "scale-100" : "scale-95"
+                    )}
+                />
+            {:else}
+                <div
+                    class="w-[40rem] h-[40rem] bg-gray-500/40 rounded-xl animate-pulse flex flex-col justify-center items-center gap-8"
+                >
+                    <Ban size={48} />
+                    <p class="text-4xl uppercase font-lobster">No cover :)</p>
+                </div>
+            {/if}
 
-    <div class="flex gap-3">
-        <button
-            on:click={resumeAudio}
-            class="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded transition"
-            aria-label="Resume"
-        >
-            ▶️ Старт
-        </button>
-        <button
-            on:click={pauseAudio}
-            class="bg-yellow-500 hover:bg-yellow-600 text-white px-4 py-2 rounded transition"
-            aria-label="Pause"
-        >
-            ⏸ Пауза
-        </button>
-        <button
-            on:click={restartAudio}
-            class="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded transition"
-            aria-label="Restart"
-        >
-            🔁 Заново
-        </button>
-    </div>
+            {@render Menu()}
+        </div>
+    </article>
 
     <div>
         <p class="text-sm text-gray-500 mt-2 break-all">
-            Текущий файл: <code>{$currentPath}</code>
+            <code class="font-marker-gothic">{$currentPath}</code>
         </p>
     </div>
 </div>
+
+{#snippet Menu()}
+    <div class="flex items-center justify-between w-full px-2">
+        <button on:click={restartAudio} aria-label="Restart">
+            <RotateCcw />
+        </button>
+
+        <div class="flex gap-3">
+            <button>
+                <SkipBack fill="currentColor" size={42} />
+            </button>
+
+            {#if $isPlaying}
+                <button on:click={pauseAudio} aria-label="Pause">
+                    <CustomCirclePause size={96} />
+                </button>
+            {:else}
+                <button on:click={resumeAudio} aria-label="Resume">
+                    <CustomCirclePlay size={96} />
+                </button>
+            {/if}
+
+            <button>
+                <SkipForward fill="currentColor" size={42} />
+            </button>
+        </div>
+
+        <button aria-label="Add to favorite">
+            <Heart />
+        </button>
+    </div>
+{/snippet}
